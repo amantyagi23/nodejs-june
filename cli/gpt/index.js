ad2c69ebb8694ea06582afddf112804397f4e3f3
amantyagi23 <amantyagi4987@gmail.com>
2024-07-17 17:07:03 +0530

    17-07

--- cli/gpt/index.js
+++ cli/gpt/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+#!/usr/bin/env node 
+
+import chalk from "chalk";
+import {program} from "commander"
+
+
+
+// commander
+// gpt - commander 
+//  information
+
+
+program.command("ask").version("1.0.0").name("Chat GPT")
+.option("-g","--greet","say hello to user")
+.description("this is a chatgpt in cli")
+
+program.parse()
+
+if(program.greet){
+    console.log("Hello user i am here to help you")
+}
